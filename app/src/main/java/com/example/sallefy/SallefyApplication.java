6f08ccf5b6ccc958a3bd105b89d0a1388c231736
Alfredo Rueda Unsain <alfredorueda@users.noreply.github.com>
2022-10-16 19:25:30 +0200

    Update SallefyApplication.java

--- app/src/main/java/com/example/sallefy/SallefyApplication.java
+++ app/src/main/java/com/example/sallefy/SallefyApplication.java
@@ -6,7 +6,7 @@ import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 public class SallefyApplication extends DaggerApplication {
-    public static final String BASE_URL = "http://sallefy.eu-west-3.elasticbeanstalk.com/api/";
+    public static final String BASE_URL = "http://apollo.eu-west-3.elasticbeanstalk.com/api/";
 
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
